3e6ada643fdf5ee0336441af7390b50e7b3ecd96
Danil-alt <kilindany24@gmail.com>
2024-11-12 03:55:05 +0500

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,6 +1,10 @@
 def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
+        if n == 0:
+            break
+        elif m == 0:
+            break
         matrix_ = []
         matrix.append(matrix_)
         for j in range(m):
@@ -11,3 +15,4 @@ def get_matrix(n, m, value):
 print(get_matrix(2, 2, 10))
 print(get_matrix(3, 5, 42))
 print(get_matrix(4, 2, 13))
+print(get_matrix(1, 0, 7))
